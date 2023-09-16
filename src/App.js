b8497dea163b9fc56d15844883022ef3ef7f433a
innasz <mini5167@naver.com>
2023-09-16 19:35:45 +0900

    add tailwind

--- src/App.js
+++ src/App.js
@@ -27,12 +27,34 @@ const App = () => {
     getMovies();
   }, []);
   return (
-    <div>
-      {movies?.map((movie, index) => (
-        <div key={index}>
-          <h1>{movie.title}</h1>
-        </div>
-      ))}
+    <div className='flex flex-row justify-center itmes-center'>
+      <div className='grid grid-cols-4 p-5 gap-4 auto-cols-max'>
+        {movies?.map((movie, index) => (
+          <div className='max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+            <img
+              class='rounded-t-lg'
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <div class='p-5'>
+              <a href='#'>
+                <h5 class='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+                  {movie.title}
+                </h5>
+              </a>
+              <p class='mb-3 font-normal text-gray-700 dark:text-gray-400'>
+                {movie.overview}
+              </p>
+              <a
+                href='#'
+                class='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+              >
+                Read more
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
