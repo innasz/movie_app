5b6565d0a2619489a81567785db585970fc792d2
innasz <mini5167@naver.com>
2023-09-16 19:05:49 +0900

    get Movie data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const options = {
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOWZhYTk0MmRkMGQzZTA0NTg4YjBkZGNjNzRjY2UyMSIsInN1YiI6IjYzYmMyZjA4Zjg1OTU4MDA4NzQ4MmMwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eXyrC0pHaFplxQNuN9BQ4YrFzPNpE4l2aolx80lC3dE',
+        },
+      };
+      const response = await axios.get(
+        'https://api.themoviedb.org/3/movie/now_playing',
+        options
+      );
+      console.log('+++++++++++++++++++++++==', response.data.results);
+      setMovies(response.data.results);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {movies?.map((movie, index) => (
+        <div key={index}>
+          <h1>{movie.title}</h1>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
